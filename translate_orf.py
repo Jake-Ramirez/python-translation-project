5e7cacec0db607b191b3e47e50fb7fd0e3c015f4
Jake-Ramirez <josepjacobo@gmail.com>
2026-04-27 12:50:57 -0500

    imports were not working because I did not omit the .py extension

--- translate_orf.py
+++ translate_orf.py
@@ -2,9 +2,9 @@
 
 import sys
 
-from find_orf.py import find_first_orf
-from translate.py import translate_sequence
-from find_orf.py import parse_sequence_from_path
+from find_orf import find_first_orf
+from translate import translate_sequence
+from find_orf import parse_sequence_from_path
  
 def main():
     import argparse
@@ -59,7 +59,14 @@ def main():
     if not args.stop_codon:
         args.stop_codon = default_stop_codons
     
-    genetic_code = {'GUC': 'V', 'ACC': 'T', 'GUA': 'V', 'GUG': 'V', 'ACU': 'T', 'AAC': 'N', 'CCU': 'P', 'UGG': 'W', 'AGC': 'S', 'AUC': 'I>
+if __name__ == '__main__':
+    genetic_code = {'GUC': 'V', 'ACC': 'T', 'GUA': 'V', 'GUG': 'V', 'ACU': 'T', 'AAC': 'N', 'CCU': 'P', 'UGG': 'W', 'AGC': 'S',
+ 'AUC': 'I', 'CAU': 'H', 'AAU': 'N', 'AGU': 'S', 'GUU': 'V', 'CAC': 'H', 'ACG': 'T', 'CCG': 'P', 'CCA': 'P', 'ACA': 'T', 'CCC': 'P',
+ 'UGU': 'C', 'GGU': 'G', 'UCU': 'S', 'GCG': 'A', 'UGC': 'C', 'CAG': 'Q', 'GAU': 'D', 'UAU': 'Y', 'CGG': 'R', 'UCG': 'S', 'AGG': 'R',
+ 'GGG': 'G', 'UCC': 'S', 'UCA': 'S', 'UAA': '*', 'GGA': 'G', 'UAC': 'Y', 'GAC': 'D', 'UAG': '*', 'AUA': 'I', 'GCA': 'A', 'CUU': 'L',
+ 'GGC': 'G', 'AUG': 'M', 'CUG': 'L', 'GAG': 'E', 'CUC': 'L', 'AGA': 'R', 'CUA': 'L', 'GCC': 'A', 'AAA': 'K', 'AAG': 'K', 'CAA': 'Q',
+ 'UUU': 'F', 'CGU': 'R', 'CGC': 'R', 'CGA': 'R', 'GCU': 'A', 'GAA': 'E', 'AUU': 'I', 'UUG': 'L', 'UUA': 'L', 'UGA': '*', 'UUC': 'F'}
+
     rna_seq = ("AUG"
             "UAC"
             "UGG"
@@ -77,7 +84,9 @@ def main():
             "AGU"
             "ACA"
             "GCG")
-
+    
+    main()
+    
     orf = find_first_orf(sequence = sequence,
             start_codons = args.start_codon,
             stop_codons = args.stop_codon)
@@ -85,5 +94,4 @@ def main():
     translated_orf = translate_sequence(rna_sequence = orf, genetic_code = genetic_code)
     sys.stdout.write('{}\n'.format(translated_orf))
 
-if __name__ == '__main__':
-    main()
+
